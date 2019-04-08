2aeb92b4304ceb0a4b58bf4d8bd946e822a7a343
Robin TROUVE <rtrouve@solutec.lan>
2019-04-08 16:23:40 +0200

    Removing Level

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LevelComponent } from './level/level.component';
 import { ChronoComponent } from './chrono/chrono.component';
 import { RandomComponent } from './random/random.component';
 
 const routes: Routes = [
   { path: 'random', component: RandomComponent},
   { path: 'chrono', component: ChronoComponent},
-  { path: 'level/:num', component: LevelComponent},
   { path: '', redirectTo: '/random', pathMatch: 'full'}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { DisplayGridComponent } from './display-grid/display-grid.component';
 import { HeaderComponent } from './header/header.component';
 
 import { AppRoutingModule } from './app-routing.module';
-import { LevelComponent } from './level/level.component';
 import { ChronoComponent } from './chrono/chrono.component';
 import { FooterComponent } from './footer/footer.component';
 import { DifficultyChoiceComponent } from './difficulty-choice/difficulty-choice.component';
@@ -18,7 +17,6 @@ import { RandomComponent } from './random/random.component';
     AppComponent,
     DisplayGridComponent,
     HeaderComponent,
-    LevelComponent,
     ChronoComponent,
     FooterComponent,
     DifficultyChoiceComponent,
